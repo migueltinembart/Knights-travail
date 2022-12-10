bca8779adba848fe78a76f8ffb43a0504fdc29d6
miguel.tinembart <miguel.tinembart@gmail.com>
2022-12-10 18:37:08 +0100

    refactor: prettier output

--- main.rb
+++ main.rb
@@ -2,7 +2,10 @@ require_relative './lib/game_board'
 
 t1 = Time.now
 game = GameBoard.new
-x = game.knight_moves([0,0], [7,3])
-p x
+start = [0,0]
+finish = [7,3]
+knight = game.knight_moves(start, finish)
+puts "You made it in #{knight.length - 1} moves!  Here's your path:"
+knight.each { |move| p move.position }
 t2 = Time.now
 puts "Time spent for this operation... #{t2 - t1}"
